7ccf3b4a2a0a1b2da781377ba6959a387c075998
NETANEL <NETANEL@NATANREU-LAP-HZ>
2016-07-17 15:57:55 +0300

    with ping in explorer

--- BlankCordovaApp3/www/scripts/index.js
+++ BlankCordovaApp3/www/scripts/index.js
@@ -2,10 +2,13 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+
 (function () {
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    //var url_media = "https://sites.google.com/site/natanmp912912/ping/ping.mp3?attredirects=0&d=1";
+    //var my_media = new Media(url_media);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
@@ -18,6 +21,12 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        var url_media = "/images/ping.mp3";
+
+        var my_media = new Media(url_media);
+        //my_media.play; 
+        console.log(my_media);
+        document.getElementById("stam button1").onclick = function () { playmedia(my_media) };
     };
 
     function onPause() {
@@ -27,4 +36,11 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
+    function playmedia(media_object) {
+
+        alert("BEFORE MEDIA  2 ");
+        media_object.play();
+        //ping1.play();
+        alert("AFTER MEDIA  ");
+    }
 } )();
